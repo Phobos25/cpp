38dc16cbc96a036d9bdbcf2d1cdfc90c1248e130
PhobosHome <fydte@mail.ru>
2021-09-26 14:59:10 +0900

    trying heavy strings

--- test/test.cpp
+++ test/test.cpp
@@ -25,21 +25,40 @@ template <typename String>
 vector<Group<String>> GroupHeavyStrings(vector<String> strings) {
   // Напишите реализацию функции,
   set<char> s;
-  map<set<char>, int> group;
+  map<set<char>, size_t> group;
+  // тут можно использовать int вместо vector<String>, но туда пихать номер
+  // группы слов, т.е. мне нужна еще 1 переменная-счетчки, которая будет учитывать
+  // номер группы стрингов
+  size_t count = 0;
+  vector<Group<String>> result; 
 
   for (const auto& word:strings){
     s.clear();
     for (const auto& letter:word){
       if (s.count(letter) == 0){
-        s.insert(letter);
-      }
-    }
-    ++group[s];
-  }
-  
-  cout << group.size();  
+        s.insert(letter);        
+      } 
+    }        
+    // TODO сделать проверку
+    if (group.count(s) == 0){
+      group[s] = count++;
+    }    
 
-  vector<Group<String>> result;
+    if (result.size() >= count){
+      result[group[s]].push_back(word);
+    } else {      
+      result.push_back({word});
+    }
+  }  
+
+  // for (auto& [key, value]:group){
+  //   Group<String> vec;
+  //   for (auto& v:value){
+  //     vec.push_back(move(v));
+  //   }
+  //   result.push_back(vec);    
+  // }
+  
   return result;
   // использовав не более 1 копирования каждого символа
 }
@@ -47,5 +66,11 @@ vector<Group<String>> GroupHeavyStrings(vector<String> strings) {
 int main (){
   vector<string> strings = {"caab", "abc", "cccc", "bacc", "c", "d", "cba"};
   auto groups = GroupHeavyStrings(strings);
+  for (auto vec:groups){
+    for (auto v:vec){
+      cout << v << ' ';
+    }
+    cout << endl;
+  }
   return 0;
 }
