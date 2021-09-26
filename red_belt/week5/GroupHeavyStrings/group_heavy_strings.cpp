a5e036e5abacbffa6397e2f47cda2fdd771fadc1
PhobosHome <fydte@mail.ru>
2021-09-27 08:30:11 +0900

    2

--- red_belt/week5/GroupHeavyStrings/group_heavy_strings.cpp
+++ red_belt/week5/GroupHeavyStrings/group_heavy_strings.cpp
@@ -20,29 +20,29 @@ using Group = vector<String>;
 // использовать Char<String>
 template <typename String>
 using Char = typename String::value_type;
-template <typename String>
-using Key = String;
 
 template <typename String>
-Key<String> ComputeStringKey (const String& string){
-  String chars = string;
-  sort(begin(chars), end(chars));
-  chars.erase(unique(begin(chars), end(chars)), end(chars));
-  return chars;
+vector<Group<String>> GroupHeavyStrings(vector<String> strings) {  
+  // Напишите реализацию функции,
+  set<char> s;
+  map<set<char>, int> group;
+
+  for (const auto& word:strings){
+    s.clear();
+    for (const auto& letter:word){
+      if (s.count(letter) == 0){
+        s.insert(letter);
+      }
+    }
+    ++group[s];
+  }
+  
+  cout << group.size();  
+
+  vector<Group<String>> result;
+  return result;
 }
 
-template <typename String>
-vector<Group<String>> GroupHeavyStrings(vector<String> strings) {
-  map<Key<String>, Group<String>> groups_map;
-  for (String& string:strings){
-    groups_map[ComputeStringKey(string)].push_back(move(string));
-  }
-  vector<Group<String>> groups;
-  for (auto& [key, group]:groups_map){
-    groups.push_back(move(group));
-  }
-  return groups;
-}
 
 void TestGroupingABC() {
   vector<string> strings = {"caab", "abc", "cccc", "bacc", "c"};
