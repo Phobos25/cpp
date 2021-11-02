888c8cd5a7874ae601900e55975db8857b823099
Phobos25 <fydte@maill.ru>
2021-11-02 14:15:54 +0900

    trying explore key words 3
    - single thread solution is done

--- red_belt/week5/ExploreKeyWords/my_solution/main.cpp
+++ red_belt/week5/ExploreKeyWords/my_solution/main.cpp
@@ -1,19 +1,41 @@
 #include "test_runner.h"
-#include "profile.h"
+//#include "profile.h"
 
 #include <map>
+#include <set>
+#include <vector>
 #include <string>
+#include <sstream>
 using namespace std;
 
 struct Stats {
   map<string, int> word_frequences;
 
-  void operator += (const Stats& other);
+  map<string, int>& operator += (const Stats& other){
+    for (auto& [key, value]:other.word_frequences){
+      if (word_frequences.count(key) != 0){
+        word_frequences[key] += value;
+      } else {
+        word_frequences[key] = value;
+      }
+    }
+    return word_frequences;
+  }
 };
 
 Stats ExploreLine(const set<string>& key_words, const string& line) {
   Stats stats;
-  
+  stringstream ss(line);
+  while (ss){
+    string word;
+    ss >> word;
+    if (key_words.find(word) != key_words.end()){
+      ++stats.word_frequences[word];
+    }
+    cout << word << " ";
+  }
+  cout << endl;
+  return stats;
 }
 
 Stats ExploreKeyWordsSingleThread(
@@ -28,12 +50,15 @@ Stats ExploreKeyWordsSingleThread(
 
 Stats ExploreKeyWords(const set<string>& key_words, istream& input) {
   // Реализуйте эту функцию
-  string word;
+  string line;
   Stats stats;
-  input >> word;
-  if (key_words.find(word) != key_words.end()){
-    ++stats.word_frequences[word];
+  while (input){
+    getline(input, line, '\n');
+    stats += ExploreLine(key_words, line);
   }
+
+
+  return stats;
 }
 
 void TestBasic() {
@@ -55,7 +80,23 @@ void TestBasic() {
   ASSERT_EQUAL(stats.word_frequences, expected);
 }
 
+void MyTest(){
+  const set<string> key_words = {"yangle", "rocks", "sucks", "all"};
+
+  stringstream ss;
+  ss << "this new yangle service really rocks\n";
+  ss << "It sucks when yangle isn't available\n";
+  ss << "10 reasons why yangle is the best IT company\n";
+  ss << "yangle rocks others suck\n";
+  ss << "Goondex really sucks, but yangle rocks. Use yangle\n";
+  const auto stats = ExploreKeyWords(key_words, ss);
+  cout << "yangle: " << stats.word_frequences.at("yangle") << endl;
+  cout << "rocks: "  << stats.word_frequences.at("rocks") << endl;
+  cout << "sucks: "  << stats.word_frequences.at("sucks") << endl;
+}
+
 int main() {
-  TestRunner tr;
-  RUN_TEST(tr, TestBasic);
+//  TestRunner tr;
+//  RUN_TEST(tr, TestBasic);
+  MyTest();
 }
